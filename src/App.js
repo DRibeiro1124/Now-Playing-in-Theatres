7e731d2e701957b959896d95e8ef1396f18b65ea
Chris <chrispackett1998@gmail.com>
2018-08-30 17:39:43 -0400

    fetched movie data and added the movies array to state

--- src/App.js
+++ src/App.js
@@ -5,17 +5,37 @@ import HomePage from './Components/HomePage';
 import NavBar from './Components/NavBar'
 
 const API_KEY = '39df1c4c7a287510f53854893ba3d788'
+const BASE_URL = 'https://api.themoviedb.org/3/movie/now_playing'
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      movies: []
+    }
+  }
+
+  componentDidMount() {
+    fetch(`${BASE_URL}?api_key=${API_KEY}&language=en-US&page=1`)
+      .then(resp => resp.json())
+      .then(movieData => {
+        this.setState({
+          movies: movieData.results
+        })
+      })
+  }
+
   render() {
     return (
       <Router>
-      <NavBar />
-      <section className="App-component">
-        <Switch>
-          <Route path="/" exact component={HomePage} />
-        </Switch>
-      </section>
+        <section>
+          <NavBar />
+          <section className="App-component">
+            <Switch>
+              <Route path="/" exact component={HomePage} />
+            </Switch>
+          </section>
+        </section>
       </Router>
     );
   }
