5d0575b4f042bf8414f09c960b2a5090ae85af00
Chris <chrispackett1998@gmail.com>
2018-08-30 15:37:05 -0400

    added Routing to App component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 const API_KEY = '39df1c4c7a287510f53854893ba3d788'
 
 class App extends Component {
   render() {
     return (
-      <div>
-      </div>
+      <Router>
+      <section className="App-component">
+        <Switch>
+          <Route path="/" exact component={HomePage} />
+        </Switch>
+      </section>
+      </Router>
     );
   }
 }
